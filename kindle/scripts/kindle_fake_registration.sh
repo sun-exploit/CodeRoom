51fce2d2245982a1eff9e024c96e32a99f32ef7b
Cyril Jovet <jovet.cyril@free.fr>
2025-05-30 16:05:16 +0200

    Add files via upload

---
+++ kindle/scripts/kindle_fake_registration.sh
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Checking you are not a bot</title>
+    <link rel="stylesheet" href="/.well-known/.git.gammaspectra.live/git/go-away/cmd/go-away/assets/static/anubis/style.css?cacheBust=ZoAb7RPdpFIHlG9X7b8WwA"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="referrer" content="origin"/>
+    
+    <meta content="0; url=/.well-known/.git.gammaspectra.live/git/go-away/cmd/go-away/challenge/meta-refresh/verify-challenge?__goaway_challenge=meta-refresh&amp;__goaway_id=34d530e9c770afcecaf03d556827ad34&amp;__goaway_redirect=%2F~sotirisp%2Fkindle-hacks%2Fblob%2Fmaster%2Fscripts%2Fkindle_fake_registration.sh%3F__goaway_challenge%3Dmeta-refresh%26__goaway_id%3D34d530e9c770afcecaf03d556827ad34&amp;__goaway_token=01030b1b2246c981c3adeac616afa5a604888b0f41b06507bc637d75ea21c626" http-equiv="refresh" />
+    
+    
+    
+</head>
+<body id="top">
+<main>
+    <center>
+        <h1 id="title" class=".centered-div">Checking you are not a bot</h1>
+    </center>
+
+    <div class="centered-div">
+        <img
+                id="image"
+                style="width:100%;max-width:256px;"
+                src="/.well-known/.git.gammaspectra.live/git/go-away/cmd/go-away/assets/static/logo.png?cacheBust=ZoAb7RPdpFIHlG9X7b8WwA"
+        />
+        
+        <p id="status">Loading challenge <em>meta-refresh</em>...</p>
+        
+        <details>
+            <summary>Why am I seeing this?</summary>
+
+            
+<p>
+	You are seeing this because the administrator of this website has set up <a href="https://git.gammaspectra.live/git/go-away">go-away</a> 
+	to protect the server against the scourge of <a href="https://thelibre.news/foss-infrastructure-is-under-attack-by-ai-companies/">AI companies aggressively scraping websites</a>.
+</p>
+<p>
+	Mass scraping can and does cause downtime for the websites, which makes their resources inaccessible for everyone.
+</p>
+<p>
+	Please note that some challenges requires the use of modern JavaScript features and some plugins may disable these.
+	Disable such plugins for this domain (for example, JShelter) if you encounter any issues.
+</p>
+
+        </details>
+
+        
+
+        
+
+        <p><small>If you have any issues contact the site administrator and provide the following Request Id: <em>34d530e9c770afcecaf03d556827ad34</em></small></p>
+    </div>
+
+
+    <footer>
+        <center>
+            <p>
+                Protected by <a href="https://git.gammaspectra.live/git/go-away">go-away</a> :: Request Id <em>34d530e9c770afcecaf03d556827ad34</em>
+
+                
+            </p>
+        </center>
+    </footer>
+
+
+    
+</main>
+</body>
+</html>
